c4e31e1530244543a90b3a6a2f00abdf53c46beb
Cappy Hausfeld <cchausfeld@gmail.com>
2023-06-20 12:46:00 -0600

    ripped everything out for demo

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,31 +1,8 @@
-import React, {useState} from "react";
+import React from "react";
 
 const Homepage = () => {
-    const [count, setCount] = useState(0)
-
-    const increment = () => setCount(count + 1)
-
-    const decrement = () => {
-        if (count > 0) {
-            setCount(count - 1)
-        }
-    }
-
-    const reset = () => {
-        setCount(0)
-
-        window.parent.postMessage('userName', '*')
-    }
-
     return (
-        <>
-            <p>Counter: {count}</p>
-            <div className='buttons'>
-                <button onClick={increment}>increment</button>
-                <button onClick={decrement}>decrement</button>
-                <button className='reset' onClick={reset}>reset</button>
-            </div>
-        </>
+        <p>In progress! Check back soon</p>
     );
 }
 
